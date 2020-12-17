17a3bbc928d3819a5b477979d28d56f5a8f0a539
M'aiq <m.janik1031@gmail.com>
2020-12-16 18:50:07 -0600

    add project files

---
+++ RemoveSillyDot/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using Deli;
+using FistVR;
+using HarmonyLib;
+
+namespace RemoveM1911TacRedDot
+{
+	public class Plugin : DeliBehaviour
+	{
+		[HarmonyPatch(typeof(HandgunSlide), "Awake")]
+		[HarmonyPostfix]
+		public static void SightRemove(HandgunSlide __instance)
+		{
+			var obj = __instance.Handgun.ObjectWrapper;
+			if (obj != null)
+			{
+				if (obj.ItemID == "M1911Tactical")
+				{
+					var slideTF = __instance.transform;
+					slideTF.Find("Sight").gameObject.SetActive(false);
+				}
+			}
+		}
+
+		void Awake()
+		{
+			Harmony.CreateAndPatchAll(typeof(Plugin));
+		}
+	}
+}
